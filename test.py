03cd8a7e73275c103059e02014de8b033e8806f6
lushuo <lushuo@kedacom.com>
2021-12-01 20:02:10 +0800

    selenium

--- test.py
+++ test.py
@@ -1,7 +1,30 @@
 from projects.excel2json import excel2json
+import os
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
 
-# excel2json(r'E:\流程表资料\问卷', r'E:\流程表资料\问卷\输出', is_format=True)
+# 获取当前脚本的绝对路径
+# print(os.path.abspath(__file__))
+# 获取当前脚本的dir路径
+# print(os.path.dirname(os.path.abspath(__file__)))
 
-from projects.qs_rank import main
+DRIVER_LOCATION = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'browser_driver')
 
-main()
+CHROME_DRIVER_LOCATION = os.path.join(os.path.join(DRIVER_LOCATION, 'chromedriver_win32'), 'chromedriver.exe')
+EDGE_DRIVER_LOCATION = os.path.join(os.path.join(DRIVER_LOCATION, 'edgedriver_win64'), 'msedgedriver.exe')
+
+print(EDGE_DRIVER_LOCATION)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--start-maximized')
+
+edge_options = webdriver.EdgeOptions()
+edge_options.add_argument('--start-maximized')
+
+ser = Service(EDGE_DRIVER_LOCATION)
+
+print(ser)
+# driver = webdriver.Chrome(executable_path=str(CHROME_DRIVER_LOCATION), options=chrome_options)
+driver = webdriver.Edge(ser, edge_options)
+print(driver)
+# driver.get('http://www.baidu.com')
