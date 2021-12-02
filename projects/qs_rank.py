0ac3f945121ba374ec181e83e1f24866cbc57c19
lushuo <lushuo@kedacom.com>
2021-12-02 19:58:02 +0800

    qs_rank_crawler

--- projects/qs_rank.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-def main():
-    r = requests.get('https://www.topuniversities.com/sites/default/files/qs-rankings-data/en/3740566.txt?1638264782?v=1638270639954')
-    print(r.json())
-
